739850ba53c0539b8742815793b952a53c7d3bfe
reyounglee <reyounglee@live.cn>
2017-06-24 01:13:37 +0800

    ie9

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@
 
 	particlesJS.load('bg', 'src/opt.json', function() {
 		console.log('callback - particles.js config loaded');
-	});
-
-	
+	});	
 })();
+
+function adaptIE(){
+	var link = document.createElement('link');
+	link.rel = 'stylesheet';
+	link.href = 'src/ie.css';
+	document.querySelector('head').appendChild(link);
+}
